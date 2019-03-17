3597d432be034a3c1014c7133705279f9ba78c17
Jarol Cruz <48305862+JarolCruz@users.noreply.github.com>
2019-03-16 22:57:51 -0600

    Primer subida de la clase estudiante - Jarol Cruz

---
+++ Estudiante.java
@@ -0,0 +1,27 @@
+package Clases;
+
+public class Estudiante extends Persona {
+
+    public void nombreEstudiante(){
+        
+        System.out.println("Juan Carlos Diaz Manzanarez");
+        
+    }
+
+    @Override
+    public void instituto() {
+        
+        instituto = "Jose Castro Lopez";
+    }
+
+    @Override
+    public void Curso() {
+       curso = 6;
+        
+    }
+    
+    
+
+
+    
+}
